b92ce2e3275511966061b2ad5b9f6217e76b2f6e
Sirat Shaikh <you@example.com>
2025-07-15 13:47:00 +0530

    first commit

---
+++ 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int num;
+    
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    if(num > 0) {
+        printf("The number is positive.\n");
+    } 
+    return 0;
+}
