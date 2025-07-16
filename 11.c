934aebcd3082b30e52320de324bab30e209cb76f
Sirat Shaikh <you@example.com>
2025-07-16 10:19:25 +0530

    first commit

---
+++ 11.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() 
+{
+    int num1, num2;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    if (num1 > num2) {
+        printf("%d is greater than %d\n", num1, num2);
+    } else if (num1 < num2) {
+        printf("%d is less than %d\n", num1, num2);
+    } else {
+        printf("Both numbers are equal.\n");
+    }
+    return 0;
+
+}
