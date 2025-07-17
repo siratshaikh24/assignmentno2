3dc157c85aab6286233f9063c5e63f81db979501
Sirat Shaikh <you@example.com>
2025-07-17 22:24:25 +0530

    first commit

---
+++ 6.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+ {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 5 == 0 || num % 7 == 0)
+        printf("Number is divisible by 5 or 7\n");
+    else
+        printf("Number is not divisible by 5 or 7\n");
+
+        return 0;
+
+}
