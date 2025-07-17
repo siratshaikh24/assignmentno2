16da6359343dba1f59f3088c5ebca343bf906a74
Sirat Shaikh <you@example.com>
2025-07-17 21:58:45 +0530

    first commit

--- 5.c
+++ 5.c
@@ -1,21 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
+int main() 
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
 
-int main()
- {
-    int a, b, c;
-
-    printf("Enter three numbers: ");
-    scanf("%d %d %d", &a, &b, &c);
-
-    if(a > b && a > c) {
-        printf("%d is the greatest.\n", a);
-    }
-    else if(b > c) {
-        printf("%d is the greatest.\n", b);
-    }
-    else {
-        printf("%d is the greatest.\n", c);
-    }
-    return 0;
-    
+    if (num % 5 == 0) {
+        if (num % 7 == 0)
+            printf("Number is divisible by both 5 and 7\n");
+        else
+            printf("Number is divisible by 5 but not by 7\n");
+    } else
+        printf("Number is not divisible by 5\n");
+        
+        return 0;
 }
