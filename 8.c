38d8ce6a3d81e08936f91bfe80e62dc94ebf88f4
Sirat Shaikh <you@example.com>
2025-07-17 22:29:27 +0530

    first commit

--- 8.c
+++ 8.c
@@ -1,16 +1,18 @@
 
-#include<stdio.h>
-int main()
- {
-    int num;
-    printf("Enter a number: ");
-    scanf("%d", &num);
+#include <stdio.h>
 
-    if (num % 5 == 0 || num % 7 == 0)
-        printf("Number is divisible by 5 or 7\n");
-    else
-        printf("Number is not divisible by 5 or 7\n");
+int main() 
+{
+    char ch;
 
-        return 0;
+    printf("Enter a lowercase character: ");
+    scanf("%c", &ch);
 
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+        printf("%c is a vowel.\n", ch);
+    } else {
+        printf("%c is a consonant.\n", ch);
+    }
+
+    return 0;
 }
