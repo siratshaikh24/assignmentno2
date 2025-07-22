e111d392fd8b8d8056e15eb9fe1d160348de2140
siratshaikh24 <siratshaikh24@gmail.com>
2025-07-22 17:03:51 +0530

    Update 20.c

--- 20.c
+++ 20.c
@@ -9,13 +9,16 @@ int main()
     if (ch >= 48 && ch <= 57) {
         printf("The character is a Digit.\n");
     }
-    else if (ch >= 65 && ch <= 90) {
+    else if (ch >= 65 && ch <= 90)
+    {
         printf("The character is an Uppercase Letter.\n");
     }
-    else if (ch >= 97 && ch <= 122) {
+    else if (ch >= 97 && ch <= 122)
+    {
         printf("The character is a Lowercase Letter.\n");
     }
-    else {
+    else
+    {
         printf("The character is neither a digit nor an alphabet.\n");
     }
     return 0;
